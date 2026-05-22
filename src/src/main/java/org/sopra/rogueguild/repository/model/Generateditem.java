adb3082cd1c53f77d3103128ad4884c3b649fe4c
Diego Vizcaí <divise2015@gmail.com>
2026-05-22 10:58:33 +0200

    Ft/generar nameitem (#17)
    * Cambio del metodo generar item porque devolvia un numero
    
    * Quitamos el exten y delvolvemos un item, en el while añadimos generar el nuevo nombre, en geenerar precio arreglamos para que la variable precio contenga cada caso
    
    * He creado la clase generated item que extienda de item para que luego cuando utilizamos generated item se genere el item
    
    ---------
    
    Co-authored-by: Diego <divise2015gmail.com>

---
+++ src/src/main/java/org/sopra/rogueguild/repository/model/Generateditem.java
@@ -0,0 +1,8 @@
+package org.sopra.rogueguild.repository.model;
+
+public class Generateditem extends Item{
+
+    public Generateditem(String name, int price, ItemCategory category) {
+        super(name, price, category);
+    }
+}
