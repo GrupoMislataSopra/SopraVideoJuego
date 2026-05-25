c52c6a8253c2ea87039d0ae3f6d62ff394135a17
Claudia Llorens Martín <clallomar2@alu.edu.gva.es>
2026-05-25 09:45:42 +0200

    Clase Quest creada, con sus debidos métodos

--- src/src/main/java/org/sopra/rogueguild/repository/ShopRepository.java
+++ src/src/main/java/org/sopra/rogueguild/repository/ShopRepository.java
@@ -8,7 +8,6 @@ import org.sopra.rogueguild.repository.model.*;
 public class ShopRepository {
     private Map<Integer, Item> stock;
     private ItemGenerator item;
-
     public ShopRepository() {
         stock = new LinkedHashMap<>();
         item= new ItemGenerator();
