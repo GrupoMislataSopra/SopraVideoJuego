a345747d700bd7b6fa66fecf5ebd726f861fcd4f
Diego Vizcaí <divise2015@gmail.com>
2026-05-27 12:45:41 +0200

    Atributo ItemEquip y sus limitaciones (#32)
    Co-authored-by: Diego <divise2015gmail.com>

--- src/src/main/java/org/sopra/rogueguild/repository/model/Player.java
+++ src/src/main/java/org/sopra/rogueguild/repository/model/Player.java
@@ -1,17 +1,28 @@
 package org.sopra.rogueguild.repository.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Player {
     private String name;
     private int gold;
     private List<Item> inventory = new ArrayList<>();
+    private static Map<ItemCategory, List<Item>>itemEquipped= new HashMap<>(Map.of(
+            ItemCategory.WEAPON, new ArrayList<>(),
+            ItemCategory.ARMOR, new ArrayList<>(),
+            ItemCategory.HELMET, new ArrayList<>(),
+            ItemCategory.BOOTS, new ArrayList<>()
+
+    ));
 
     public Player(String name, int gold) {
         this.name = name;
         this.gold = gold;
+
     }
+
 
     public String getName() { return name; }
     public int getGold() { return gold; }
@@ -38,6 +49,15 @@ public class Player {
         int actualAmount = Math.min(amount, space);
         this.gold += actualAmount;
         return actualAmount;
+    }
+    
+    public int limitEquip(ItemCategory category){
+        return switch (category){
+            case WEAPON -> 2;
+            case ARMOR, HELMET, BOOTS -> 1;
+            default -> 0;
+
+        };
     }
 
 
