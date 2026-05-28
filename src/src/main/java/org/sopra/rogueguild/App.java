6d1824e770fe363206a6c6d955228c1a5deae850
Diego Vizcaí <divise2015@gmail.com>
2026-05-28 15:14:00 +0200

    Ft/usuario puede elegir rol (#39)
    * Añadimos un scanner a la case APP para pedir el nombre del jugador
    
    * e añadido la clase enum con el get de los tipos de enum para añadir al player el tipo de rol que es, ahora el rol tmb sale en el view
    
    ---------
    
    Co-authored-by: Diego <divise2015gmail.com>

--- src/src/main/java/org/sopra/rogueguild/App.java
+++ src/src/main/java/org/sopra/rogueguild/App.java
@@ -4,6 +4,7 @@ import org.sopra.rogueguild.controller.ShopController;
 import org.sopra.rogueguild.repository.QuestRepository;
 import org.sopra.rogueguild.repository.ShopRepository;
 import org.sopra.rogueguild.repository.model.Player;
+import org.sopra.rogueguild.repository.model.PlayerRol;
 import org.sopra.rogueguild.view.ViewDisplay;
 
 import java.util.Scanner;
@@ -16,8 +17,31 @@ public class App {
         ViewDisplay view = new ViewDisplay();
 
         System.out.println("Escribe tu nombre  del perspnaje:");
-        String nombre= sc.nextLine().trim();
-        Player player = new Player( nombre, 250);
+        String name= sc.nextLine().trim();
+        System.out.println("Elige tu rol de personaje:");
+        System.out.println("1.Warrior");
+        System.out.println("2.Mage");
+        System.out.println("3.Rogue");
+        System.out.println("4.Archer");
+        int option= sc.nextInt();
+        PlayerRol playerRol = null;
+        switch (option){
+            case 1:
+                playerRol=PlayerRol.WARRIOR;
+                break;
+            case 2:
+                playerRol=PlayerRol.MAGE;
+                break;
+            case 3:
+                playerRol=PlayerRol.ROGUE;
+                break;
+            case 4:
+                playerRol=PlayerRol.ARCHER;
+                break;
+        }
+
+
+        Player player = new Player( name, 250,playerRol);
 
         ShopController controller = new ShopController(player, view, repository, questRepository);
         controller.start();
