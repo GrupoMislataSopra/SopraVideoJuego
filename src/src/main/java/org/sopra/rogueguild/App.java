f32e5d67cff38056af7620013df3687b14fcf5e0
Diego <divise2015gmail.com>
2026-05-28 12:16:03 +0200

    Añadimos un scanner a la case APP para pedir el nombre del jugador

--- src/src/main/java/org/sopra/rogueguild/App.java
+++ src/src/main/java/org/sopra/rogueguild/App.java
@@ -6,13 +6,18 @@ import org.sopra.rogueguild.repository.ShopRepository;
 import org.sopra.rogueguild.repository.model.Player;
 import org.sopra.rogueguild.view.ViewDisplay;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         ShopRepository repository = new ShopRepository();
         QuestRepository questRepository = new QuestRepository();
         ViewDisplay view = new ViewDisplay();
 
-        Player player = new Player( "Iñigo Montolla", 250);
+        System.out.println("Escribe tu nombre  del perspnaje:");
+        String nombre= sc.nextLine().trim();
+        Player player = new Player( nombre, 250);
 
         ShopController controller = new ShopController(player, view, repository, questRepository);
         controller.start();
